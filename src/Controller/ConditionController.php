229c24cbf872a340c32458f09d963e0768963082
siriiana <sirine.benghazala@lespoles.org>
2025-01-07 11:36:35 +0100

    upupupup

--- src/Controller/ConditionController.php
+++ src/Controller/ConditionController.php
@@ -8,28 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ConditionController extends AbstractController
 {
-    #[Route('/cvg', name: 'app_cvg')]
-    public function index(): Response
-    {
-        return $this->render('condition/index.html.twig', [
-            'controller_name' => 'ConditionController',
-        ]);
-    }
-}
-
-
-
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class ConditionController extends AbstractController
-{
-    #[Route('/cgv', name: 'app_cgv')]
+    #[Route('/cgv', name: 'app_condition')]
     public function index(): Response
     {
         return $this->render('condition/index.html.twig', [
