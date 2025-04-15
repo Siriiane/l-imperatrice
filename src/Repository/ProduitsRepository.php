dabdec8f53ed03f94b4a0a6431fd5a7630243475
siriiane <sirine.benghazala@lepoles.org>
2025-04-15 16:59:50 +0200

    com15

--- src/Repository/ProduitsRepository.php
+++ src/Repository/ProduitsRepository.php
@@ -16,28 +16,28 @@ class ProduitsRepository extends ServiceEntityRepository
         parent::__construct($registry, Produits::class);
     }
 
-    //    /**
-    //     * @return Produits[] Returns an array of Produits objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+       /**
+        * @return Produits[] Returns an array of Produits objects
+        */
+       public function findByExampleField($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.exampleField = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
 
-    //    public function findOneBySomeField($value): ?Produits
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+       public function findOneBySomeField($value): ?Produits
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.exampleField = :val')
+               ->setParameter('val', $value)
+               ->getQuery()
+               ->getOneOrNullResult()
+           ;
+       }
 }
